5a4e9cb490246953a3e1592216ca034afe6aa806
Tomislav Pesut <tomca32@gmail.com>
2015-08-22 20:25:35 -0400

    Added basic fields for movie

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from movie import Movie
 
-Movie()
+my_movie = Movie("Some Movie", "Some Description", "IMAGE", "YOUTUBE")
+print (my_movie.title, my_movie.description, my_movie.cover_image, my_movie.trailer_link)
--- movie.py
+++ movie.py
@@ -1,3 +1,6 @@
 class Movie():
-  def __init__(self):
-    print("Initialized")
+  def __init__(self, title, description, cover_image, trailer_link):
+    self.title = title
+    self.description = description
+    self.cover_image = cover_image
+    self.trailer_link = trailer_link
