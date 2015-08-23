e8ef5b76d6bf344f5aef755c5213b96590d234d9
Tomislav Pesut <tomca32@gmail.com>
2015-08-22 20:34:24 -0400

    Added some real movies

--- movie.py
+++ movie.py
@@ -1,6 +1,6 @@
 class Movie():
-  def __init__(self, title, description, cover_image, trailer_link):
+  def __init__(self, title, description, cover_image, trailer_url):
     self.title = title
     self.description = description
     self.cover_image = cover_image
-    self.trailer_link = trailer_link
+    self.trailer_url = trailer_url
